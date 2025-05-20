12736f5d2fe620f441fc566da76c01e94146199c
Bilen M <bilenmehalek7@gmail.com>
2025-05-21 00:19:43 +0300

    removed profile

--- apps/profile/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ProfileModule } from './profile.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ProfileModule);
-  await app.listen(process.env.port ?? 3005);
-  console.log('🚀 Profile Microservice is running on http://localhost:3005');
-}
-bootstrap();
